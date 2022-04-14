89ffb531fdc219c42fb9ac18c1bd29b6c130f2e5
Tian Pan <puncsky@gmail.com>
2022-04-14 02:30:02 -0700

    clean up

--- config/development.js
+++ config/development.js
@@ -1,13 +1,7 @@
 module.exports = {
   indexer: {
     catchup: {
-      enabled: true,
-      blocksBatchSize: 1,
-
-      blockNumberRanges: [
-        [170040, 170040],
-        [170498, 170498],
-      ],
+      enabled: false,
     },
     realtime: {
       enabled: false,

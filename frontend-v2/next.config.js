b2b500ac6450c1a682574d90709c20d34bf8c8b2
Dora Noda <dev@stargately.com>
2024-03-29 09:51:16 -0700

    update

--- frontend-v2/next.config.js
+++ frontend-v2/next.config.js
@@ -2,9 +2,6 @@ const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  distDir: "build",
-
   reactStrictMode: true,
   swcMinify: true,
   // i18n,
